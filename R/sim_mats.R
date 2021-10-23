6cebc0d716a2c64050c83bee0556954e916ffbda
mtrupiano1 <mtrupiano1@gmail.com>
2021-10-23 10:55:39 -0400

    Add functions for S_x and S_w

--- R/sim_mats.R
+++ R/sim_mats.R
@@ -104,3 +104,69 @@ StMatrixCalc <- function(v){
   stmat <- 1/(1+dtmat)
   return(stmat)
 }
+
+
+
+#' Calculate Similarity Matrix for Exogenous Predictors
+#'
+#' Largely a wrapper function for base R's \code{stats::dist()} function. First calculates
+#' nxn distance matrix using specified method for an input matrix or vector using
+#'  \code{stats::dist()}. Then converts the distance matrix to Similarity matrix using 1/(D_x +1)
+#'
+#' @param A matrix
+#' @param XdistMetric character
+#'
+#' @return matrix
+#' @export
+#'
+#' @examples
+#' X <- matrix(c(1,1,1,2,2,2,3,3,3),nrow=3,ncol=3,byrow=TRUE)
+#' SxMatrixCalc(X,'euclidean')
+SxMatrixCalc <- function(A,XdistMetric='euclidean'){
+  dxmat <- as.matrix(stats::dist(A,method=XdistMetric ,upper=TRUE,diag=TRUE))
+  sxmat <- 1/(1+dxmat)
+  return(sxmat)
+}
+
+
+
+#' Calculate Weighted Similarity Matrix
+#'
+#' @param t.in numeric vector of time orders for points in the response series
+#' @param p.in numeric vector of period within a seasonal cycle (ex. 1 for January points in monthly data)
+#' @param nPeriods.in numeric scalar indicating the maximum value \code{p.in} could take on (ex. 12 for monthly data)
+#' @param X.in numeric vector or matrix of exogenous predictors, where the rows correspond to points in the response series
+#' @param XdistMetric.in Distance calculation method to be used by \code{stats::dist()}
+#' @param weights numeric vector where first value represents weight for \code{S_t}, second value the weight for \code{S_p}, and the third value the weight for \code{S_x}
+#'
+#' @return numeric matrix which is the weighted similarity matrix \code{S_w}
+#' @export
+#'
+#' @examples
+#' t <- c(1,2,3)
+#' p <- c(1,2,1)
+#' X <- matrix(c(1,1,1,2,2,2,3,3,3),nrow=3,ncol=3,byrow=TRUE)
+#' SwMatrixCalc(t.in=t,p.in=p,nPeriods.in = 2,X.in=X
+#'              ,XdistMetric.in = "euclidean",weights=c(1/4,1/4,1/2))
+SwMatrixCalc<- function(t.in,p.in,nPeriods.in,X.in,XdistMetric.in='euclidean',weights=c(1/3,1/3,1/3)){
+  alpha <- weights[1]
+  beta <- weights[2]
+  gamma <- weights[3]
+
+  St <- StMatrixCalc(v=t.in)
+
+  Sp <- SpMatrixCalc(v=p.in,nPeriods=nPeriods.in)
+
+  Sx <- SxMatrixCalc(A=X.in,XdistMetric=XdistMetric.in)
+
+  Sw <- alpha*St + beta*Sp + gamma*Sx
+
+  return(Sw)
+
+}
+
+
+
+
+
+
