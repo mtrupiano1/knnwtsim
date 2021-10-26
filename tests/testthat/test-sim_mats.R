dbbf62eacc33fbd1aa4be09fdf0bb455709bcd7d
mtrupiano1 <mtrupiano1@gmail.com>
2021-10-26 16:59:45 -0400

    Writing first unit test

--- tests/testthat/test-sim_mats.R
+++ tests/testthat/test-sim_mats.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("SeasonalAbsDistance works when around and direct dist are needed", {
+  px <- 1
+  py <- 6
+  pz <- 3
+  np <- 7
+
+  #Test of around dist
+  expect_equal(SeasonalAbsDistance(px,py,np), 2)
+
+  #Test of direct dist
+  expect_equal(SeasonalAbsDistance(px,pz,np), 2)
 })
